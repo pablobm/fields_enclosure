07e2a34c5f7eed321596d1b3f65e876844a32cde
pablobm <pablobm@gmail.com>
2009-02-17 18:14:25 +0000

    Test updated for compatibility with 2.2

--- test/form_helper_test.rb
+++ test/form_helper_test.rb
@@ -50,34 +50,49 @@ class FormHelperTest < ActionView::TestCase
     end
     @controller = @controller.new
   end
+  
+  EXPECTED_OUTPUT = "<form action='http://www.example.com' id='create-account' method='post'>" +
+    "<p><input name='account[holder_name]' size='30' type='text' id='account_holder_name' value='John Doe' /></p>" +
+    "<p><input name='account[number]' size='30' type='text' id='account_number' value='12345678' /></p>" +
+    "<p class='sort_code_triplet containsErrors'>" +
+      "<input name='account[sort_code1]' size='30' type='text' id='account_sort_code1' value='12' />" +
+      "<div class='fieldWithErrors'><input name='account[sort_code2]' size='30' type='text' id='account_sort_code2' value='34' /></div>" +
+      "<input name='account[sort_code3]' size='30' type='text' id='account_sort_code3' value='56' />" +
+    "</p>" +
+    "<p><input name='commit' id='account_submit' type='submit' value='Save account details' /></p>" +
+  "</form>"
     
-  def test_form_for
-    _erbout = ''
-  
-    form_for(:account, @account, :html => { :id => 'create-account' }) do |f|
-      f.enclosure(:p){ _erbout.concat f.text_field(:holder_name)}
-      f.enclosure(:p){ _erbout.concat f.text_field(:number)}
-      f.enclosure(:p, :class => 'sort_code_triplet') do
-        _erbout.concat f.text_field(:sort_code1)
-        _erbout.concat f.text_field(:sort_code2)
-        _erbout.concat f.text_field(:sort_code3)
+  if (ActionPack::VERSION::STRING.split('.') <=> '2.2'.split('.')) < 0
+    def test_form_for_pre_rails_2_1
+      _erbout = ''
+      form_for(:account, @account, :html => { :id => 'create-account' }) do |f|
+        f.enclosure(:p){ _erbout.concat f.text_field(:holder_name)}
+        f.enclosure(:p){ _erbout.concat f.text_field(:number)}
+        f.enclosure(:p, :class => 'sort_code_triplet') do
+          _erbout.concat f.text_field(:sort_code1)
+          _erbout.concat f.text_field(:sort_code2)
+          _erbout.concat f.text_field(:sort_code3)
+        end
+        f.enclosure(:p){ _erbout.concat f.submit('Save account details')}
       end
-      f.enclosure(:p){ _erbout.concat f.submit('Save account details')}
+
+      assert_dom_equal EXPECTED_OUTPUT, _erbout
     end
+  else
+    def test_form_for
+      form_for(:account, @account, :html => { :id => 'create-account' }) do |f|
+        f.enclosure(:p){ concat f.text_field(:holder_name)}
+        f.enclosure(:p){ concat f.text_field(:number)}
+        f.enclosure(:p, :class => 'sort_code_triplet') do
+          concat f.text_field(:sort_code1)
+          concat f.text_field(:sort_code2)
+          concat f.text_field(:sort_code3)
+        end
+        f.enclosure(:p){ concat f.submit('Save account details')}
+      end
   
-    expected =
-      "<form action='http://www.example.com' id='create-account' method='post'>" +
-        "<p><input name='account[holder_name]' size='30' type='text' id='account_holder_name' value='John Doe' /></p>" +
-        "<p><input name='account[number]' size='30' type='text' id='account_number' value='12345678' /></p>" +
-        "<p class='sort_code_triplet containsErrors'>" +
-          "<input name='account[sort_code1]' size='30' type='text' id='account_sort_code1' value='12' />" +
-          "<div class='fieldWithErrors'><input name='account[sort_code2]' size='30' type='text' id='account_sort_code2' value='34' /></div>" +
-          "<input name='account[sort_code3]' size='30' type='text' id='account_sort_code3' value='56' />" +
-        "</p>" +
-        "<p><input name='commit' id='account_submit' type='submit' value='Save account details' /></p>" +
-      "</form>"
-  
-    assert_dom_equal expected, _erbout
+      assert_dom_equal EXPECTED_OUTPUT, output_buffer
+    end
   end
   
   
